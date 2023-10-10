cec80e883b56ca7c4fa7b3fe57b0eda9ce07cc0f
GabrielEtolo <lefould@gmail.com>
2023-10-10 16:16:27 +0200

    probleme de bouton

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="d-flex flex-column h-100">
+    <header>G.E.N</header>
+
+    <main class="flex-shrink-0 pb-5">
+      <div class="container">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img
+          src="../assets/giphy.gif"
+          class="d-block mx-lg-auto img-fluid"
+          alt="Bootstrap Themes"
+          width=auto
+          height= 20%
+          loading="lazy"
+        />
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3">Gabriel Etolo Nguini</h1>
+        <p class="lead">
+          <h2>WebDesigner,UX/UI Designer,VueJs Develloper</h2>
+          Plongez dans l'univers de la conception web et du développement front-end avec un 
+          professionnel passionné : moi, un Web Designer, UX/UI Designer et développeur Vue. 
+          Formé en Design web et guidé par une curiosité insatiable, je suis déterminé à créer 
+          des interfaces intuitives et esthétiques pour répondre aux besoins des utilisateurs. 
+          Rejoignez-moi dans cette aventure passionnante où la créativité et la technologie se 
+          rejoignent pour façonner l'avenir du web.
+        </p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+            <button class="btn btn-primary"  type="submit">Button</button>
+            <button class="btn btn-primary" type="submit">Button</button>
+            
+           
+
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
