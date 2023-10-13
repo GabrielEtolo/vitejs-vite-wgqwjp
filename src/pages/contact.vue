145da09955a29217ce6e3f4bb6bbcf0ccc6a4070
GabrielEtolo <lefould@gmail.com>
2023-10-13 17:00:49 +0200

    wip

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,79 +1,69 @@
 <template>
   <section class="mb-4">
-    <!--Section heading-->
+    <!-- Section heading -->
     <h2 class="h1-responsive font-weight-bold text-center my-4">Contact</h2>
-    <!--Section description-->
+    <!-- Section description -->
     <p class="text-center w-responsive mx-auto mb-5">
       Vous avez des questions ? Contactez-moi, je me ferais un plaisir de vous
-      repondre.
+      répondre.
     </p>
 
     <div class="row contactform">
-      <!--Grid column-->
+      <!-- Grid column -->
       <div class="col-md-9 mb-md-0 mb-5">
-        <form
-          id="contact-form"
-          name="contact-form"
-          action="mail.php"
-          method="POST"
-        >
-          <!--Grid row-->
+        <form @submit.prevent="submitForm">
+          <!-- Grid row -->
           <div class="row contactform">
-            <!--Grid column-->
+            <!-- Grid column -->
             <div class="col-md-6">
               <div class="md-form mb-0">
                 <input
+                  v-model="formData.name"
                   type="text"
-                  id="name"
-                  name="name"
                   placeholder="Votre Nom"
                   class="form-control"
                 />
               </div>
             </div>
-            <!--Grid column-->
+            <!-- Grid column -->
 
-            <!--Grid column-->
+            <!-- Grid column -->
             <div class="col-md-6">
               <div class="md-form mb-0">
                 <input
+                  v-model="formData.email"
                   type="text"
-                  id="email"
-                  name="email"
                   class="form-control"
                   placeholder="Votre email"
                 />
               </div>
             </div>
-            <!--Grid column-->
+            <!-- Grid column -->
           </div>
-          <!--Grid row-->
+          <!-- Grid row -->
 
-          <!--Grid row-->
+          <!-- Grid row -->
           <div class="row contactform">
             <div class="col-md-12">
               <div class="md-form mb-0">
                 <input
+                  v-model="formData.subject"
                   type="text"
-                  id="subject"
-                  name="subject"
                   class="form-control"
                   placeholder="Votre sujet"
                 />
               </div>
             </div>
           </div>
-          <!--Grid row-->
+          <!-- Grid row -->
 
-          <!--Grid row-->
+          <!-- Grid row -->
           <div class="row contactform">
-            <!--Grid column-->
+            <!-- Grid column -->
             <div class="col-md-12">
               <div class="md-form">
                 <textarea
-                  type="text"
-                  id="message"
-                  name="message"
+                  v-model="formData.message"
                   rows="2"
                   class="form-control md-textarea"
                   placeholder="Dites m'en plus"
@@ -81,21 +71,17 @@
               </div>
             </div>
           </div>
-          <!--Grid row-->
+          <!-- Grid row -->
+
+          <div class="text-center text-md-left" style="margin-top: 1rem">
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+          </div>
+          <div class="status">{{ formStatus }}</div>
         </form>
-
-        <div class="text-center text-md-left" style="margin-top: 1rem">
-          <a
-            class="btn btn-primary"
-            onclick="document.getElementById('contact-form').submit();"
-            >Envoyer</a
-          >
-        </div>
-        <div class="status"></div>
       </div>
-      <!--Grid column-->
+      <!-- Grid column -->
 
-      <!--Grid column-->
+      <!-- Grid column -->
       <div class="col-md-3 text-center">
         <ul class="list-unstyled mb-0">
           <li>
@@ -114,7 +100,44 @@
           </li>
         </ul>
       </div>
-      <!--Grid column-->
+      <!-- Grid column -->
     </div>
   </section>
 </template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      formData: {
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      },
+      formStatus: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      const { name, email, subject, message } = this.formData;
+      if (name && email && subject && message) {
+        // You can send the form data to your server using Axios or any other HTTP library.
+        // Example using Axios:
+        axios
+          .post("your_server_endpoint", this.formData)
+          .then((response) => {
+            this.formStatus = "Email sent!";
+          })
+          .catch((error) => {
+            this.formStatus = "Error sending email.";
+          });
+
+        // For demonstration, let's assume the form is sent successfully.
+      } else {
+        this.formStatus = "Veuillez remplir le formulaire";
+      }
+    },
+  },
+};
+</script>
